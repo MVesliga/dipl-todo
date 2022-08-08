6f085b61d6cadf0df21fb6d43fd879c45f682fb2
MVesliga <martxyx@gmail.com>
2022-08-08 18:48:10 +0200

    Dodano prikazivanje liste korisnika

--- src/main/java/hr/diplomski/todo/controller/UserController.java
+++ src/main/java/hr/diplomski/todo/controller/UserController.java
@@ -1,12 +1,27 @@
 package hr.diplomski.todo.controller;
 
+import hr.diplomski.todo.converter.UserConverter;
+import hr.diplomski.todo.domain.HrUser;
+import hr.diplomski.todo.facade.UserFacade;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    UserFacade userFacade;
+    private UserConverter userConverter;
+
+    public UserController(final UserFacade userFacade,
+                          final UserConverter userConverter) {
+        this.userFacade = userFacade;
+        this.userConverter = userConverter;
+    }
 
     @GetMapping("/my-profile")
     public String getMyUserProfilePage() {
@@ -14,7 +29,12 @@ public class UserController {
     }
 
     @GetMapping("/list")
-    public String getUserList() {
+    public String getUserList(final Model model) {
+        List<HrUser> userList = userFacade.getUserRepository().findAll();
+
+        model.addAttribute("userList", userList);
         return "userList";
     }
+
+
 }
--- src/main/java/hr/diplomski/todo/repository/UserRepository.java
+++ src/main/java/hr/diplomski/todo/repository/UserRepository.java
@@ -4,10 +4,11 @@ import hr.diplomski.todo.domain.HrUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<HrUser, Integer> {
-
     HrUser findByUsername(String username);
+    List<HrUser> findAll();
 }
